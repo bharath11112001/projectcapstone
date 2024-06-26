2e4880f5e2079fe33bcf8873ef933b8d4fbb6a27
bharath11112001 <162092691+bharath11112001@users.noreply.github.com>
2024-06-26 16:22:23 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,15 +1,14 @@
+
 #!/bin/bash
 git_branch=$(git rev-parse --abbrev-ref HEAD)
+
 if [[ $git_branch == "main" ]]; then
-  ./build.sh
-  docker tag mynginximg bharath883/prod:latest
-  docker push bharath883/prod:latest
-elif [[ $git_branch == "dev" ]]; then
-  ./build.sh
-  docker tag mynginximg bharath883/dev:latest
-  docker push bharath883/dev:latest
+  echo "Deploying for main branch"
+  # Add your deployment steps for main branch here
+elif [[ $git_branch == "Dev" ]]; then
+  echo "Deploying for Dev branch"
+  # Add your deployment steps for Dev branch here
 else
   echo "Deployment failed: Unsupported branch '$git_branch'"
   exit 1
 fi
-
