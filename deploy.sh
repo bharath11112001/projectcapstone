d533097a71c57efc34538930a3d1b2bc2ccc70d9
bharath11112001 <princebharath883@gmail.com>
2024-06-26 03:13:35 +0000

    pushing

--- deploy.sh
+++ deploy.sh
@@ -3,7 +3,7 @@ git_branch=$(git rev-parse --abbrev-ref HEAD)
 if [[ $git_branch == "main" ]]; then
   ./build.sh
   docker tag mynginximg bharath883/prod:latest
-  docker push $bharath883/prod:latest
+  docker push bharath883/prod:latest
 elif [[ $git_branch == "dev" ]]; then
   ./build.sh
   docker tag mynginximg bharath883/dev:latest
