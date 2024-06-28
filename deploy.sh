e6c4ff75eaef693df8cd4b52a38aed0be2f4210d
bharath11112001 <162092691+bharath11112001@users.noreply.github.com>
2024-06-28 09:57:30 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,20 +1,19 @@
 #!/bin/bash
-BRANCH_NAME=$1
 
-echo "Current Git Branch: ${BRANCH_NAME}"
-docker-compose down
-docker system prune -f && docker volume prune -f && docker network prune -f && docker image prune -f && docker container prune -f
-if [[ "${BRANCH_NAME}" == "origin/Prod" ]]; then
-     ./build.sh 
-     docker tag mynginximg bharath883/prod:latest
-     docker push bharath883/prod:latest
-elif [[ "${BRANCH_NAME}" == "origin/Dev" ]]; then
-      
-       ./build.sh
-        docker tag mynginximg bharath883/dev:latest
-        docker push bharath883/dev:latest
+# Determine the branch name from environment variables or fallback to current Git branch
+GIT_BRANCH=${GIT_BRANCH:-$(git rev-parse --abbrev-ref HEAD)}
+
+# Define image repository
+if [[ $GIT_BRANCH == "main" ]]; then
+  IMAGE_TAG="bharath883/prod:latest"
+elif [[ $GIT_BRANCH == "dev" ]]; then
+  IMAGE_TAG="bharath883/dev:latest"
 else
-    echo "Deployment Failed ${BRANCH_NAME}"
-    exit 1
+  echo "Deployment failed: Unsupported branch '$GIT_BRANCH'"
+  exit 1
 fi
-docker-compose up -d
+
+# Perform build and push
+./build.sh
+docker tag mynginximg "$IMAGE_TAG"
+docker push "$IMAGE_TAG"
