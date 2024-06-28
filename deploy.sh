eb7dbbc44b3922f94b60df3169f1522721b58d4d
bharath11112001 <162092691+bharath11112001@users.noreply.github.com>
2024-06-28 07:47:34 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,14 +1,21 @@
 #!/bin/bash
-git_branch=$(git rev-parse --abbrev-ref HEAD)
-if [[ $git_branch == "main" ]]; then
-  ./build.sh
-  docker tag mynginximg bharath883/prod:latest
-  docker push bharath883/prod:latest
-elif [[ $git_branch == "dev" ]]; then
-  ./build.sh
-  docker tag mynginximg bharath883/dev:latest
-  docker push bharath883/dev:latest
+BRANCH_NAME=$1
+
+echo "Current Git Branch: ${BRANCH_NAME}"
+docker-compose down
+docker system prune -f && docker volume prune -f && docker network prune -f
+echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
+if [[ "${BRANCH_NAME}" == "origin/Prod" ]]; then
+     ./build.sh 
+     docker tag mynginximg bharath883/prod:latest
+     docker push bharath883/prod:latest
+elif [[ "${BRANCH_NAME}" == "origin/Dev" ]]; then
+      
+       ./build.sh
+        docker tag mynginximg bharath883/dev:latest
+        docker push bharath883/dev:latest
 else
-  echo "Deployment failed '$git_branch'"
-  exit 1
+    echo "Deployment Failed ${BRANCH_NAME}"
+    exit 1
 fi
+docker-compose up -d
